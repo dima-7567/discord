6284b6b487251e9b9a9a2be1e92721df9c53fc78
Student <e>
2023-03-04 19:34:05 +0300

    04.03.2023

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+import asyncio
+from typing import Any
+
 import discord
+from discord import client
+from discord.ext import commands
+import random, logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 TOKEN = "MTA4MTU4OTcwMjczOTYyODEwNA.G-PPP1.5ZpVhdf07IiuzwwzQ9p1RGH26o7WtFKHpm5P44"
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+class YLBotClient(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @bot.command()
+    async def foo(self, arg):
+        await self.bot.send(arg)
+
+
+async def main():
+    await bot.add_cog(YLBotClient(bot))
+    await bot.start(TOKEN)
+
+
+asyncio.run(main())
+
